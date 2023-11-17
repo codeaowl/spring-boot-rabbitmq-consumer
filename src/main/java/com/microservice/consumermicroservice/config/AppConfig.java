4ab4cc37dc9ff2cbcd74f989d90cf8d95fea0f0e
codeaowl <dejanpetrovicp@outlook.com>
2023-11-17 03:34:52 +0100

    fix: add RedisPassword package

--- src/main/java/com/microservice/consumermicroservice/config/AppConfig.java
+++ src/main/java/com/microservice/consumermicroservice/config/AppConfig.java
@@ -3,6 +3,7 @@ package com.microservice.consumermicroservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
